f20dd40f12cf18456e0f07c46f3e320ce3b99903
mcventur <marcelinopenide@gmail.com>
2023-11-27 20:30:10 +0100

    Ok navegación sin opcion arriba

--- app/src/main/java/com/example/ejemplonavigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/ejemplonavigationcomponent/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.ejemplonavigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ejemplonavigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Seteamos nuestra ToolBar como la Action Bar de la aplicación
+        setSupportActionBar(binding.topAppBar)
+
+        //Esto rompe si nuestro Nav Host es un FragmentContauinerView. Lo sustituyo por las dos líneas de debajo
+        //val navController = findNavController(R.id.nav_host)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_profile ->
+                findNavController(R.id.nav_host).navigate(R.id.profileFragment)
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        return true
+    }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
+
+
+
+
+
+
 }
